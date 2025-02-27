39f013928d0c71daf523f6da19fc3202fba3a550
RuseBa <rusedef@gmail.com>
2025-02-27 20:24:45 +0200

    Başlangıç layout dosyasındaki font dosya yüklemeleri büyük boyutlu olduğu için timeout oluyordu. Linke tıklarken anında açılması için kaldırıldı.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import { Inter } from 'next/font/google';
-import RootLayoutClient from '@/app/RootLayoutClient';
 import { AuthProvider } from './context/AuthContext';
-
-// Font tanımlamaları
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-  display: 'swap',
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-  display: 'swap',
-});
-
-const inter = Inter({ 
-  subsets: ['latin'],
-  display: 'swap',
-});
 
 export const metadata: Metadata = {
   title: "DX-HRSAM",
@@ -36,15 +13,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body 
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        suppressHydrationWarning
-      >
+    <html lang="en">
+      <body>
         <AuthProvider>
-          <RootLayoutClient>
-            {children}
-          </RootLayoutClient>
+          {children}
         </AuthProvider>
       </body>
     </html>
